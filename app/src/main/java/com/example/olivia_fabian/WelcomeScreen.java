4c28e6d701dc8e0f6b66115df2481122d55d3469
Fabian Svensson <fabian.sitges@gmail.com>
2019-05-23 13:58:44 +0200

    Progressbar added

--- app/src/main/java/com/example/olivia_fabian/WelcomeScreen.java
+++ app/src/main/java/com/example/olivia_fabian/WelcomeScreen.java
@@ -4,19 +4,30 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class WelcomeScreen extends AppCompatActivity {
+    private ProgressBar pgsBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        pgsBar = (ProgressBar) findViewById(R.id.pBar);
+        pgsBar.setVisibility(View.VISIBLE);
+
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             public void run() {
+                createIntent();
+                pgsBar.setVisibility(View.GONE);
             }
         }, 2000);
+    }
+
+    public void createIntent() {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
         finish();
