d6ad345854464eab1b6cf95eb30d69719ba9215c
Fabian Svensson <fabian.sitges@gmail.com>
2019-06-02 15:27:56 +0200

    Added a toolbar

--- app/src/main/java/com/example/olivia_fabian/Login.java
+++ app/src/main/java/com/example/olivia_fabian/Login.java
@@ -3,6 +3,9 @@ package com.example.olivia_fabian;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +28,17 @@ public class Login extends AppCompatActivity {
         email = findViewById(R.id.loginemail);
         register = findViewById(R.id.loginregister);
 
+        Toolbar my_toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(my_toolbar);
+
+        /*
+        android.support.v7.app.ActionBar actionbar = getSupportActionBar();
+        actionbar.setLogo(R.drawable.logo);
+        actionbar.setDisplayUseLogoEnabled(true);
+        actionbar.setDisplayUseLogoEnabled(true);
+        actionbar.setTitle("Log In");
+        */
+
         enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,6 +52,26 @@ public class Login extends AppCompatActivity {
             }
         });
 
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return true;
+    }
+    //and this to handle actions
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.logo) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void intentCreate() {
