edd96aff0733112edf4edc2e00e62ca5d915737b
Olivia Rey <olivia5959@gmail.com>
2019-06-05 14:59:23 +0200

    Clases de vistas

--- app/src/main/java/com/example/olivia_fabian/FlatsAdapter.java
+++ app/src/main/java/com/example/olivia_fabian/FlatsAdapter.java
@@ -12,6 +12,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.LinkedList;
 import java.util.List;
 
 public class FlatsAdapter extends ArrayAdapter<Flat> {
@@ -19,6 +23,7 @@ public class FlatsAdapter extends ArrayAdapter<Flat> {
     Button button;
     private Flat flat;
     private final FlatsScreen main;
+    LinkedList<Flat> flats;
 
     public FlatsAdapter(Context context, List<Flat> collection, FlatsScreen main) {
         super(context, R.layout.flats, collection);
@@ -44,8 +49,11 @@ public class FlatsAdapter extends ArrayAdapter<Flat> {
 
 
             ImageView f_logo = (ImageView) row.findViewById(R.id.flatpicture);
-            f_logo.setBackgroundResource(flat.getImg());
-            f_logo.setBackgroundResource(R.drawable.logo);
+
+            Picasso.get()
+                    .load("https://img3.idealista.com/blur/WEB_DETAIL_TOP-XL-L/0/id.pro.es.image.master/27/a3/e4/275711097.jpg")
+                    .into(f_logo);
+
 
             ImageView f_like = (ImageView) row.findViewById(R.id.likeicon);
             f_like.setBackgroundResource(flat.getLike());
--- app/src/main/java/com/example/olivia_fabian/FlatsScreen.java
+++ app/src/main/java/com/example/olivia_fabian/FlatsScreen.java
@@ -1,24 +1,38 @@
 package com.example.olivia_fabian;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class FlatsScreen extends AppCompatActivity {
     ListView lv;
     Object item;
+    ImageView activityMain;
 
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flats);
+        Context context = getApplicationContext();
+        ImageView imageView , aux;
+        int id = 1;
+        List<Flat> deviceCollection = new ArrayList<>();//aqui ens ho hem de descarregar tot de la api i posarho
 
-        List<Flat> deviceCollection = new ArrayList<>();
+        activityMain = findViewById(R.id.flatpicture);
 
 
         deviceCollection.add(new Flat(100, "Samsung", 1, R.drawable.ic_launcher_foreground, R.drawable.like));
@@ -39,6 +53,10 @@ public class FlatsScreen extends AppCompatActivity {
                 item = lv.getItemAtPosition(position);
             }
         });
+
+
+
+
 
     }
 
